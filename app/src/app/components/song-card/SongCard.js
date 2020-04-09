02424efc85b42b616c671c05b29a9e55922f836a
Lasse Nordahl <lasseanordahl@gmail.com>
2020-04-08 19:54:53 -0700

    made the url's better follow rest specs

--- app/src/app/components/song-card/SongCard.js
+++ app/src/app/components/song-card/SongCard.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./SongCard.css";
-import SkeletonPulse from "../skeleton-pulse/SkeletonPulse";
 
+<<<<<<< HEAD
+=======
+import { Link } from "react-router-dom";
+
+import SkeletonPulse from "../skeleton-pulse/SkeletonPulse";
+import { isOverCardLimit } from "../../../global/helper";
+
+>>>>>>> made the url's better follow rest specs
 function SongCard(props) {
   return (
     <div
@@ -18,31 +25,31 @@ function SongCard(props) {
           )}
         </div>
         <div className="song-card-info">
-          <h2>
-            {props.skeletonPulse === undefined ? (
-              props.song.name
-            ) : (
-              <SkeletonPulse style={{ width: "128px", height: "24px" }} />
-            )}
-          </h2>
-          <p>
-            {props.skeletonPulse === undefined ? (
-              props.song.artists.map(function (artist, index) {
-                return index < props.song.artists.length - 1
-                  ? artist.name + ", "
-                  : artist.name;
-              })
-            ) : (
-              <SkeletonPulse style={{ width: "200px", height: "20px" }} />
-            )}
-          </p>
-          <p className="subtitle">
-            {props.skeletonPulse === undefined ? (
-              "Playlist Count: 18"
-            ) : (
-              <SkeletonPulse style={{ width: "81px", height: "12px" }} />
-            )}
-          </p>
+          {props.skeletonPulse === undefined ? (
+            <h2>{props.song.name}</h2>
+          ) : (
+            <SkeletonPulse style={{ width: "128px", height: "24px", marginBottom: "8px" }} />
+          )}
+          <div className={isOverCardLimit(props.song.name) ? "shift-card-content": "no-shift-content"}>
+          {props.skeletonPulse === undefined ? (
+            <Link to={"/app/artists/" + props.song.artists[0].id} >
+              <p>
+                {props.song.artists.map(function (artist, index) {
+                  return index < props.song.artists.length - 1
+                    ? artist.name + ", "
+                    : artist.name;
+                })}
+              </p>
+            </Link>
+          ) : (
+            <SkeletonPulse style={{ width: "200px", height: "20px", marginBottom: "8px" }} />
+          )}
+          {props.skeletonPulse === undefined ? (
+            <p className="subtitle">Playlist Count: 18</p>
+          ) : (
+            <SkeletonPulse style={{ width: "81px", height: "12px", marginBottom: "8px" }} />
+          )}
+          </div>
         </div>
       </div>
     </div>
