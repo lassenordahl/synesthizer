70dca23b0f41a7d2a41f8b9e972705f72077218d
Zachary Pinto <zpinto@uci.edu>
2020-04-26 19:03:42 -0700

    add number of items per page toggle

--- app/src/app/components/paginate/Paginate.js
+++ app/src/app/components/paginate/Paginate.js
@@ -4,11 +4,22 @@ import "./Paginate.css";
 import { Card } from "../../containers";
 
 function Paginate(props) {
+  const [limit, setLimit] = useState(props.params.limit);
+
+  useEffect(() => {
+    props.setParams({
+      ...props.params,
+      offset: props.params.offset,
+      limit: limit,
+    });
+  }, [limit]);
+
   function leftClick() {
-    if (props.params.offset / props.params.limit + 1 > 1) {
+    if (props.params.offset / limit + 1 > 1) {
       props.setParams({
         ...props.params,
-        offset: props.params.offset - props.params.limit,
+        offset: props.params.offset - limit,
+        limit: limit,
       });
     }
   }
@@ -16,25 +27,54 @@ function Paginate(props) {
   function rightClick() {
     props.setParams({
       ...props.params,
-      offset: props.params.offset + props.params.limit,
+      offset: props.params.offset + limit,
+      limit: limit,
     });
   }
 
   return (
-    <Card
-      className="paginate-filter"
-      innerStyle={{
-        display: "flex",
-        flexDirection: "row",
-        margin: "8px 24px 8px 24px",
-      }}
-    >
-      <div className="paginate-filter-controls">
-        <p onClick={leftClick}>{"<"} </p>
-        <p>{props.params.offset / props.params.limit + 1}</p>
-        <p onClick={rightClick}> {">"}</p>
+    <div className="page-controls">
+      <div className="page-controls-paginate-filter">
+        <Card
+          innerStyle={{
+            display: "flex",
+            flexDirection: "row",
+            margin: "8px 24px 8px 24px",
+          }}
+        >
+          <div className="page-controls-paginate-filter-controls">
+            <p onClick={leftClick}>{"<"} </p>
+            <p>{props.params.offset / limit + 1}</p>
+            <p onClick={rightClick}> {">"}</p>
+          </div>
+        </Card>
       </div>
-    </Card>
+      <div className="page-controls-limit-filter">
+        <Card
+          innerStyle={{
+            width: "30px",
+            display: "flex",
+            flexDirection: "column",
+            margin: "8px 10px 8px 10px",
+          }}
+        >
+          <select
+            value={limit}
+            onChange={(e) => setLimit(e.currentTarget.value)}
+          >
+            <option key={20} value={20}>
+              20
+            </option>
+            <option key={60} value={60}>
+              40
+            </option>
+            <option key={80} value={80}>
+              80
+            </option>
+          </select>
+        </Card>
+      </div>
+    </div>
   );
 }
 
