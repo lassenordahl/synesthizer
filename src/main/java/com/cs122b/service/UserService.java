9df9b7537953853ec35a69a2fe187bc91d4e6bc7
Zachary Pinto <zpinto@uci.edu>
2020-04-27 03:31:21 -0700

    implement put endpoint to update user info

--- src/main/java/com/cs122b/service/UserService.java
+++ src/main/java/com/cs122b/service/UserService.java
@@ -20,7 +20,6 @@ public class UserService {
     }
 
     private static void insertUser(User user) throws SQLException {
-        db = new SQLClient();
 
         String insertQuery = "INSERT INTO user(id, first_name, last_name, address, email, password) "
                 + "VALUES(DEFAULT,?,?,?,?,?);";
@@ -71,6 +70,68 @@ public class UserService {
         insertUser(user);
 
         db.closeConnection();
+        return user;
+    }
+
+    public static User updateUser(JsonObject userJson) throws SQLException {
+        db = new SQLClient();
+
+        // Create user object
+        User user = new User();
+
+        user.setId(userJson.get("user_id").getAsInt());
+        user.setFirst_name(userJson.get("first_name").getAsString());
+        user.setLast_name(userJson.get("last_name").getAsString());
+        user.setAddress(userJson.get("address").getAsString());
+        user.setEmail(userJson.get("email").getAsString());
+
+        String email = user.getEmail();
+        // Check if exists (return null if exists)
+        Query query = db.query(String.format("SELECT * FROM user WHERE email='%s' AND id <> %d", email, user.getId()));
+        ResultSet result = query.getResult();
+        if (result.next() != false) {
+            return null;
+        }
+
+        query.closeQuery();
+
+        if (userJson.get("password") != null) {
+            user.setPassword(userJson.get("password").getAsString());
+        }
+
+        StringBuilder updateQuery = new StringBuilder();
+        updateQuery.append("UPDATE user SET first_name = ?, last_name = ?, address = ?, email = ? ");
+
+        if (user.getPassword() != null) {
+            updateQuery.append(", password = ? ");
+        }
+
+        updateQuery.append("WHERE id = " + user.getId() + ";");
+
+        PreparedStatement pstmt = db.getConnection().prepareStatement(updateQuery.toString(),
+                Statement.RETURN_GENERATED_KEYS);
+
+        pstmt.setString(1, user.getFirst_name());
+        pstmt.setString(2, user.getLast_name());
+        pstmt.setString(3, user.getAddress());
+        pstmt.setString(4, user.getEmail());
+
+        if (user.getPassword() != null) {
+            pstmt.setString(5, user.getPassword());
+        }
+
+        int affectedRows = pstmt.executeUpdate();
+
+        if (affectedRows > 0) {
+            // get the ID back
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+        }
+
+        pstmt.close();
+
         return user;
     }
 
