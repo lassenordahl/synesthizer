0ee3f5b5d94c2292085f040ae5d5aa6155465c59
Lasse Nordahl <lasseanordahl@gmail.com>
2020-04-27 21:57:22 -0700

    update uri

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -3,7 +3,8 @@ import {
   history
 } from "../index"
 
-let baseUrl = "http://127.0.0.1:8080/unnamed/api"
+let baseUrl = process.env.REACT_APP_API_URL || "http://localhost:8080/unnamed/api";
+// let baseUrl = "http://127.0.0.1:8080/unnamed/api"
 
 axios.interceptors.response.use(undefined, error => {
   if (error.response !== undefined) {
