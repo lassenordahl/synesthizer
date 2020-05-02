520763e4c65f31ed0506f603191180bc8b55675e
Lasse Nordahl <lasseanordahl@gmail.com>
2020-05-01 22:20:56 -0700

    progress on recaptcha stuff

---
+++ app/src/app/components/recaptcha/Recaptcha.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Recaptcha() {
+  return (
+    <div ref={this.handleRecaptchaRef} data-sitekey="6LecPfEUAAAAAKjBkF-aqpo56xWnLuQG0pjqK-Uc"/>
+  );
+}
+
+export default Recaptcha;
