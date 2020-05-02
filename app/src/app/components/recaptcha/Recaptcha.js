5a5225ecb4a27258fb8030aadf3f6308bf13a8d4
Lasse Nordahl <lasseanordahl@gmail.com>
2020-05-02 12:06:42 -0700

    recaptcha stuff

--- app/src/app/components/recaptcha/Recaptcha.js
+++ app/src/app/components/recaptcha/Recaptcha.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Recaptcha() {
+import { Button } from "../../components";
+
+function Recaptcha(props) {
+
+  const [captcha, setCaptcha] = useState(null);
+  const [widgetId, setWidgetId] = useState(1);
+
+  function handleRecaptchaRef(elem) {
+    setCaptcha(elem);
+  }
+
+  function handleRecaptchaSubmit() {
+    debugger;
+    if (props.grecaptcha && props.grecaptcha.getResponse() === "") {
+
+    } else {
+      props.handleSubmit(props.onSubmit)
+    }
+    // props.onSubmit(props.grecaptcha.getResponse());
+  }
+
+  function execute() {
+    if (props.grecaptcha) {
+      props.grecaptcha.reset(widgetId);
+    }
+  }
+
   return (
-    <div ref={this.handleRecaptchaRef} data-sitekey="6LecPfEUAAAAAKjBkF-aqpo56xWnLuQG0pjqK-Uc"/>
+    <React.Fragment>
+      <div ref={handleRecaptchaRef} className="g-recaptcha" data-sitekey="6LecPfEUAAAAAKjBkF-aqpo56xWnLuQG0pjqK-Uc"/>
+      <Button
+        type="submit"
+        style={{ width: "65px", height: "40px" }}
+        isPrimary={true}
+        onClick={() => handleRecaptchaSubmit()}
+      >
+        {props.action}
+      </Button>
+    </React.Fragment>
   );
 }
 
--- app/src/app/components/recaptcha/RecaptchaWrapper.js
+++ app/src/app/components/recaptcha/RecaptchaWrapper.js
@@ -1,9 +1,17 @@
-import recaptcha from "./Recaptcha";
+import React from "react";
+
+import Recaptcha from "./Recaptcha";
 import makeAsyncScriptLoader from "react-async-script";
 
 let url = "https://www.google.com/recaptcha/api.js?onload=grecaptcha&render=explicit";
 
+function RecaptchaWrapper(props) {
+  return (
+    <Recaptcha handleSubmit={props.handleSubmit}></Recaptcha>
+  )
+}
+
 export default makeAsyncScriptLoader(url, {
   callbackName: "onloadcallback",
   globalName: "grecaptcha"
-})(recaptcha);
+})(RecaptchaWrapper);
