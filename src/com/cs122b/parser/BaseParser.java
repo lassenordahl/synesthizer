43e0386227ef530860087889d46786fa8981c5ee
Zachary Pinto <zpinto@uci.edu>
2020-05-10 15:06:48 -0700

    add duplicate detection, missing data detection, and report generation

--- src/com/cs122b/parser/BaseParser.java
+++ src/com/cs122b/parser/BaseParser.java
@@ -1,13 +1,64 @@
 package com.cs122b.parser;
 
+import java.util.HashSet;
+
 import com.mysql.jdbc.Statement;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 class BaseParser {
-    BaseParser() {
+    int dupCount;
+    int missingDataCount;
+    StringBuilder report;
+    Set<String> dupSet;
 
+    BaseParser() {
+        this.dupCount = 0;
+        this.missingDataCount = 0;
+        this.report = new StringBuilder();
+        this.dupSet = new HashSet<String>();
+    }
+
+    void addDupSet(String identifier) {
+        this.dupSet.add(identifier);
+    }
+
+    Boolean isDuplicate(String identifier, String toString) {
+        if (this.dupSet.contains(identifier)) {
+            this.dupCount++;
+            this.report.append("[Duplicate] ");
+            this.report.append(toString + "\n");
+        }
+    }
+
+    int getFailCount(int[] rows) {
+        int failure = 0;
+        for (int row : rows) {
+            if (row == Statement.EXECUTE_FAILED)
+                failure++;
+        }
+        return failure;
+    }
+
+    int getSuccessCount(int[] rows) {
+        int success = 0;
+        for (int row : rows) {
+            if (row == Statement.SUCCESS_NO_INFO)
+                success++;
+        }
+        return success;
+    }
+
+    String generateReport(String type, int[] rows) {
+        this.report.append("----------------------------------");
+        this.report.append("------------Statistics------------");
+        this.report.append("----------------------------------");
+        this.report.append(String.format("%ss Parsed: %d", type, rows.length));
+        this.report.append(String.format("%ss Inserted: %d", type, this.getSuccessCount(rows)));
+        this.report.append(String.format("%ss Duplicates: %d", type, this.dupCount));
+        this.report.append(String.format("%ss Missing Data: %d", type, this.missingDataCount));
+        return this.report.toString();
     }
 
     String getTextValue(Element ele, String tagName) {
@@ -39,24 +90,6 @@ class BaseParser {
             return 0;
         }
         return Float.parseFloat(getTextValue(ele, tagName));
-    }
-
-    int getFailCount(int[] rows) {
-        int failure = 0;
-        for (int row : rows) {
-            if (row == Statement.EXECUTE_FAILED)
-                failure++;
-        }
-        return failure;
-    }
-
-    int getSuccessCount(int[] rows) {
-        int success = 0;
-        for (int row : rows) {
-            if (row == Statement.SUCCESS_NO_INFO)
-                success++;
-        }
-        return success;
     }
 
 }
