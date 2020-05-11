8cfc4f13533c332b3e97036d5e7f2ddfb23c6938
Zachary Pinto <zpinto@uci.edu>
2020-05-11 10:58:06 -0700

    fix xml

--- data_collection_creation/xml_creator.py
+++ data_collection_creation/xml_creator.py
@@ -17,18 +17,26 @@ ALBUM_DATA_FILENAME = 'collected_data/album_data.json'
 
 WRITE_PERM = 'w+'
 
-def create_xml(data_file_name, data_key, xml_name):
+def create_xml(data_file_name, data_key, xml_name, keys):
     with open(data_file_name, 'r') as items_data:
         items = json.load(items_data)
         items = items[data_key]
-        xml = dicttoxml.dicttoxml(items, custom_root=xml_name)
+
+        clean_items = []
+        for item in items:
+            if len(keys) == 0:
+                clean_items.append(item)
+            else:
+                clean_items.append({key: item[key] for key in keys})
+
+        xml = dicttoxml.dicttoxml(clean_items, custom_root=xml_name)
         dom = parseString(xml)
         with open('xml_creation/{}.xml'.format(xml_name), WRITE_PERM) as xml_file:
             xml_file.write(dom.toprettyxml())
 
 
 if __name__ == '__main__':
-    create_xml(ARTIST_DATA_FILENAME, 'artist_details', 'artists')
-    create_xml(ALBUM_DATA_FILENAME, 'album_details', 'albums')
-    create_xml(SONG_DETAILS_FILENAME, 'track_details', 'tracks')
-    create_xml(TRACK_INFO_FILENAME, 'track_information', 'track_metas')
+    create_xml(ARTIST_DATA_FILENAME, 'artist_details', 'artists', ["id", "name", "images", "genres"])
+    create_xml(ALBUM_DATA_FILENAME, 'album_details', 'albums', ["id", "name", "album_type", "release_date", "images", "artists"])
+    create_xml(SONG_DETAILS_FILENAME, 'track_details', 'tracks', ["id", "name", "track_number", "album"])
+    create_xml(TRACK_INFO_FILENAME, 'track_information', 'track_metas', [])
